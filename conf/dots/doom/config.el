955a043503feeffa644414b1cc652d49bb8ab6ed
danielefongo <danielefongo@gmail.com>
2022-02-11 19:03:30 +0100

    replace neovim with emacs (default config)

---
+++ conf/dots/doom/config.el
@@ -0,0 +1,10 @@
+(setq user-full-name "Daniele Fongo"
+      user-mail-address "danielefongo@gmail.com")
+
+(setq doom-theme 'doom-one
+      doom-font (font-spec :family "Hasklig" :size 18)
+      doom-big-font (font-spec :family "Hasklig" :size 24))
+
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type t)
--- conf/dots/zsh/aliases.zsh
+++ conf/dots/zsh/aliases.zsh
@@ -1,4 +1,3 @@
-alias vim="nvim"
 alias events='icalbuddy -f -sc -ss "" -npn -nc -iep "title,datetime" -ps "| : |" -po "datetime,title" eventsToday+10'
 alias tup='sudo networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 9050 off; sudo networksetup -setsocksfirewallproxystate Wi-Fi on; tor'
 alias toff='sudo networksetup -setsocksfirewallproxystate Wi-Fi off'
